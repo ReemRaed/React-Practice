c0842d6e469c1e6df91d5850a26670b1042965c5
Reem Raed <119124998+ReemRaed@users.noreply.github.com>
2024-11-23 13:51:28 +0200

    Delete contextAPI/context-api directory

--- contextAPI/context-api/src/constants/DUMMY-USERS.js
+++
@@ -1,9 +0,0 @@
-const dummyUsers = [
-    { id: 1, name: "Alice", age: 28 },
-    { id: 2, name: "Bob", age: 34 },
-    { id: 3, name: "Charlie", age: 22 },
-    { id: 4, name: "David", age: 45 },
-    { id: 5, name: "Eve", age: 30 }
-];
-
-export default dummyUsers;
